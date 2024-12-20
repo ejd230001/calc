27ba7b17651cf5b3b05967a5eb51fa237efc5673
Elden Deguia <eldendeguia@outlook.com>
2024-12-19 21:38:26 -0600

    Added CSS

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Initialize required variables
 let storedNum = NaN;
 let operatorJustClicked = false;
 let currentOperator = NaN;
 
+// Select DOM elements
 const screen = document.querySelector(".screen");
 const numbers = document.querySelectorAll(".number");
 const clear = document.querySelector(".AC");
@@ -13,12 +15,12 @@ const add = document.querySelector(".plus");
 const decimal = document.querySelector(".decimal");
 const equal = document.querySelector(".equal");
 
+// Add appropriate event listeners
 numbers.forEach((number) => {
     number.addEventListener("click", () => numberClicked(number.textContent))
 })
 
 clear.addEventListener("click", allClear);
-
 add.addEventListener("click", () => operatorSelected(1));
 subtract.addEventListener("click", () => operatorSelected(2));
 multiply.addEventListener("click", () => operatorSelected(3));
@@ -26,7 +28,7 @@ divide.addEventListener("click", () => operatorSelected(4))
 modulus.addEventListener("click", () => operatorSelected(5))
 equal.addEventListener("click", evaluate);
 
-
+// Number is clicked, appends in to current number on screen or replaces number on screen if operator was just clicked
 function numberClicked(num) {
     if (!operatorJustClicked) {
         screen.textContent = screen.textContent + num;
@@ -37,18 +39,22 @@ function numberClicked(num) {
     }
 }
 
+// Clear all text and stored numbers
 function allClear() {
     screen.textContent = "";
     storedNum = NaN;
     operatorJustClicked = false;
 }
 
+// Operator is clicked, set current operation appropriately and store number that was already on screen
 function operatorSelected(operator) {
+    storedNum = screen.textContent;
     operatorJustClicked = true;
     currentOperator = operator;
-    storedNum = screen.textContent;
+    
 }
 
+// Evaluate once equals is pressed, performs operation on number stored when operator was clicked and number currently on screen
 function evaluate() {
     let numToOperate = screen.textContent;
 
@@ -71,10 +77,14 @@ function evaluate() {
             }
             else
                 screen.textContent = Number(storedNum) / Number(numToOperate);
-                operatorJustClicked = true;
+            operatorJustClicked = true;
             break;
         case 5:
-            screen.textContent = Number(storedNum) % Number(numToOperate);
+            if (numToOperate == 0) {
+                screen.textContent = "gc lil bro";
+            }
+            else
+                screen.textContent = Number(storedNum) % Number(numToOperate);
             operatorJustClicked= true;
             break;
     }
